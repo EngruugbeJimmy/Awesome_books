b083f72f4050b1c94619f0acfa60e35f7afba99e
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-01 14:34:22 +0100

    Create localStorage function and update index.js
    Co-authored-by: abahuugbede_dev <89143961+EngruugbeJimmy@users.noreply.github.com>
    Co-authored-by: Oscah Deus <pipkillerinfo@gmail.com>

--- js/index.js
+++ js/index.js
@@ -4,65 +4,73 @@ const bookList = document.querySelector('.book__list');
 const addNewBookForm = document.querySelector('.new__book');
 
 // Create an book collection for storing books
-const booksCollector = [
-  {
-    title: 'Things Fall Apart',
-    author: 'Chinua Achebe',
-  },
-  {
-    title: 'Woman, Thou Art Loosed',
-    author: 'T. D. Jakes',
-  },
-];
+let booksCollector = [];
+
+// Create a function to add Collection to local storage
+const AddLocalStorage = (data) => {
+  localStorage.setItem('booksCollection', JSON.stringify(data));
+};
 
 // Add display function
 const displayBook = (arr) => {
   bookList.innerHTML = '';
 
-  // Loop through the array given
-  arr.forEach((book, id) => {
-    // create a book card with a class 'book'
-    const bookdiv = document.createElement('div');
-    bookdiv.className = 'book';
-
-    // create the book title element with class 'book__title'
-    const bookTitle = document.createElement('p');
-    bookTitle.className = 'book__title';
-    bookTitle.innerText = book.title;
-
-    // create the book author element with class 'book__author'
-    const bookAuthor = document.createElement('p');
-    bookAuthor.className = 'book__author';
-    bookAuthor.innerText = book.author;
-
-    // Create the delete button with a class 'book__remove-btn'
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'book__remove-btn';
-    deleteBtn.innerText = 'Remove';
-
-    // Attach a listener to the remove botton for delete function
-    deleteBtn.addEventListener('click', () => {
-      deleteBook(id);
-    });
-
-    // Create an horizonal line element
-    const hrLine = document.createElement('hr');
-
-    // Append all created elements to the book card
-    bookdiv.appendChild(bookTitle);
-    bookdiv.appendChild(bookAuthor);
-    bookdiv.appendChild(deleteBtn);
-    bookdiv.appendChild(hrLine);
+  if (arr.length === 0) {
+    // create a empty message element with a class 'book__empty'
+    const emptyMsg = document.createElement('p');
+    emptyMsg.className = 'book__empty';
+    emptyMsg.innerText = 'Empty Book Collection';
 
     // Append the book card to the parent node
-    bookList.appendChild(bookdiv);
-  });
+    bookList.appendChild(emptyMsg);
+  } else {
+    // Loop through the array given
+    arr.forEach((book, id) => {
+      // create a book card with a class 'book'
+      const bookdiv = document.createElement('div');
+      bookdiv.className = 'book';
+
+      // create the book title element with class 'book__title'
+      const bookTitle = document.createElement('p');
+      bookTitle.className = 'book__title';
+      bookTitle.innerText = book.title;
+
+      // create the book author element with class 'book__author'
+      const bookAuthor = document.createElement('p');
+      bookAuthor.className = 'book__author';
+      bookAuthor.innerText = book.author;
+
+      // Create the delete button with a class 'book__remove-btn'
+      const deleteBtn = document.createElement('button');
+      deleteBtn.className = 'book__remove-btn';
+      deleteBtn.innerText = 'Remove';
+
+      // Attach a listener to the remove botton for delete function
+      deleteBtn.addEventListener('click', () => {
+        deleteBook(id);
+      });
+
+      // Create an horizonal line element
+      const hrLine = document.createElement('hr');
+
+      // Append all created elements to the book card
+      bookdiv.appendChild(bookTitle);
+      bookdiv.appendChild(bookAuthor);
+      bookdiv.appendChild(deleteBtn);
+      bookdiv.appendChild(hrLine);
+
+      // Append the book card to the parent node
+      bookList.appendChild(bookdiv);
+    });
+  }
 };
 
 // Create a remove function
 const deleteBook = (index) => {
+  booksCollector = JSON.parse(localStorage.getItem('booksCollection'));
   booksCollector.splice(index, 1);
   displayBook(booksCollector);
+  AddLocalStorage(booksCollector);
 };
 
 // Create a add book function
@@ -79,6 +87,7 @@ const addBook = (formElement) => {
   // add book object to book collection
   booksCollector.push(newBook);
   displayBook(booksCollector);
+  AddLocalStorage(booksCollector);
 };
 
 // add event listener to form
@@ -90,5 +99,6 @@ addNewBookForm.addEventListener('submit', (event) => {
 
 // Add event listener to window reload
 window.addEventListener('load', () => {
+  booksCollector = JSON.parse(localStorage.getItem('booksCollection'));
   displayBook(booksCollector);
 });
