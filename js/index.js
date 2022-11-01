d9ca5a420e31b47e4e3af370f092c30857cf4564
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-01 13:36:52 +0100

    Create a disply function and a remove function
    Co-authored-by: abahuugbede_dev <89143961+EngruugbeJimmy@users.noreply.github.com>
    Co-authored-by: Oscah Deus <pipkillerinfo@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,30 +1,71 @@
+/* eslint-disable no-use-before-define */
 // js to access html elements
-const books = document.querySelector('.books__container');
-const addForm = document.querySelectorAll('.add__book');
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
+const bookList = document.querySelector('.book__list');
+/*
+const addNewBookForm = document.querySelector('.new__book');
+*/
+// Create an book collection for storing books
 const booksCollector = [
   {
-    title: 'Lorem ipsum',
-    author: 'Testeroo Testyy',
+    title: 'Things Fall Apart',
+    author: 'Chinua Achebe',
   },
   {
-    title: 'Second book',
-    author: 'Testeroo Testyy',
+    title: 'Woman, Thou Art Loosed',
+    author: 'T. D. Jakes',
   },
-
 ];
 
-// Create a remove function
+// Add display function
+const displayBook = (arr) => {
+  bookList.innerHTML = '';
 
-// Create array from the books variable
-const currentBooks = Array.from(books.querySelectorAll('.book'));
+  // Loop through the array given
+  arr.forEach((book, id) => {
+    // create a book card with a class 'book'
+    const bookdiv = document.createElement('div');
+    bookdiv.className = 'book';
 
-// Add event listeners to the button of the book cards
-currentBooks.forEach((book, index) => {
-  book.addEventListener('click', (event) => {
-    if (event.target.matches('button')) {
-      books.removeChild(books.children[index]);
-    }
+    // create the book title element with class 'book__title'
+    const bookTitle = document.createElement('p');
+    bookTitle.className = 'book__title';
+    bookTitle.innerText = book.title;
+
+    // create the book author element with class 'book__author'
+    const bookAuthor = document.createElement('p');
+    bookAuthor.className = 'book__author';
+    bookAuthor.innerText = book.author;
+
+    // Create the delete button with a class 'book__remove-btn'
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'book__remove-btn';
+    deleteBtn.innerText = 'Remove';
+
+    // Attach a listener to the remove botton for delete function
+    deleteBtn.addEventListener('click', () => {
+      deleteBook(id);
+    });
+
+    // Create an horizonal line element
+    const hrLine = document.createElement('hr');
+
+    // Append all created elements to the book card
+    bookdiv.appendChild(bookTitle);
+    bookdiv.appendChild(bookAuthor);
+    bookdiv.appendChild(deleteBtn);
+    bookdiv.appendChild(hrLine);
+
+    // Append the book card to the parent node
+    bookList.appendChild(bookdiv);
   });
+};
+
+// Create a remove function
+const deleteBook = (index) => {
+  booksCollector.splice(index, 1);
+  displayBook(booksCollector);
+};
+
+window.addEventListener('load', () => {
+  displayBook(booksCollector);
 });
